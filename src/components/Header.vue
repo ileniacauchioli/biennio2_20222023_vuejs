c2ed9432813ae10ce170ca93dc09452dbf120296
ileniacauchioli <127940239+ileniacauchioli@users.noreply.github.com>
2023-06-20 17:03:45 +0200

    Modifiche header

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -5,15 +5,19 @@
 <template>
    
   <header>
-    <div class="wrapper">
-      <h1>My Portfolio</h1>
-      <H2>Fotorafia</H2>
-      <p>
-        Ho da sempre nutrito un profondo amore per la fotografia e la sua  capacità di immortalare istanti che rimarranno indelebili nel tempo.
-  Amo trascorrere del tempo con le persone, perché sono convinta che ognuna di esse possa regalarmi qualcosa di unico, qualcosa che porterò con me.
-  La mia passione per la fotografia mi ha spinto a intraprendere un percorso accademico, dove ho potuto approfondire le tecniche, le teorie, le conoscenze e i segreti di questa meravigliosa forma d'arte. 
-  Sono sempre alla ricerca di nuove sfide e di modi creativi per esprimere la mia passione attraverso l'obiettivo.</p>
-   <img src="@/assets/Foto_portfolio.jpg" alt="Profilo" width="180" height="250"/>
+    <div class="wrapper d-flex">
+      <div class="row justify-content-start">
+        <h1>My Portfolio</h1>
+        <H2>Fotorafia</H2>
+        <p class="w-75 p-3">
+          Ho da sempre nutrito un profondo amore per la fotografia e la sua  capacità di immortalare istanti che rimarranno indelebili nel tempo.
+    Amo trascorrere del tempo con le persone, perché sono convinta che ognuna di esse possa regalarmi qualcosa di unico, qualcosa che porterò con me.
+    La mia passione per la fotografia mi ha spinto a intraprendere un percorso accademico, dove ho potuto approfondire le tecniche, le teorie, le conoscenze e i segreti di questa meravigliosa forma d'arte. 
+    Sono sempre alla ricerca di nuove sfide e di modi creativi per esprimere la mia passione attraverso l'obiettivo.</p>
+      </div>
+  <div class="col">
+    <img src="@/assets/Foto_portfolio.jpg" alt="Profilo" width="220" height="280"/>
+  </div>
     </div>
   </header>
 </template>
@@ -29,8 +33,7 @@ header{
   height: 100px;
 }
 .wrapper{
-  position: relative;
-  padding-right: 30%;
-  margin-top: 2rem;
+  margin-top: 4rem;
+  margin-bottom: 4rem;
 }
 </style>
